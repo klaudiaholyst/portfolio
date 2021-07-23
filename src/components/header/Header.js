e73b83c6e7492f6e7350d0939311b676590d9e11
Klaudia <kklaudiaw@gmail.com>
2021-07-23 23:44:54 +0200

    active links underline in navigation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {useHistory} from 'react-router-dom';
+import {useHistory, NavLink} from 'react-router-dom';
 
 import styles from './Header.module.css';
 
@@ -13,10 +13,10 @@ const Header = () => {
             <div onClick={ () => history.push('/') } className={styles.name}><p>Klaudia Holyst</p></div>
             <div className={styles.menu}>
                 <ul className={styles.list}>
-                    <li onClick={ () => history.push('/') } className={styles.item}>Home</li>
-                    <li onClick={ () => history.push('/projects') } className={styles.item}>Projects</li>
-                    <li onClick={ () => history.push('/about') } className={styles.item}>About me</li>
-                    <li onClick={ () => history.push('/contact') }><button className={styles.button}>Contact</button></li>
+                    <li><NavLink exact to='/' activeClassName={styles.selected} className={styles.item}>Home</NavLink></li>
+                    <li><NavLink to='/projects' activeClassName={styles.selected} className={styles.item}>Projects</NavLink></li>
+                    <li><NavLink to='/about' activeClassName={styles.selected} className={styles.item}>About me</NavLink></li>
+                    <li><NavLink to='/contact'><button className={styles.button}>Contact</button></NavLink></li>
                 </ul>
             </div>
             
