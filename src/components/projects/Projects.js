013b4905ea3cb870e755a36763104e1c6db2fceb
Klaudia <kklaudiaw@gmail.com>
2021-07-29 23:02:41 +0200

    changed buttons styling and added active links to projects
    changed buttons styling and added active links to projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -13,7 +13,9 @@ const myProjects = [
         list: ['Authentication using Firebase. User is able to create a profile, login, logout and change profile’s email address or password.', 'Uploading photos from local computer.',
             'Pictures are saved on Firestore Database.'],
         tools: 'React.js, react-router-dom, npm, Material UI, firebase',
-        img: 'album'
+        img: 'album',
+        codeURL: 'https://github.com/klaudiaholyst/pictures-collection-app',
+        liveURL: 'https://auth-development-2a4ee.web.app/'
     },
     {
         title: 'Contacts App',
@@ -21,31 +23,40 @@ const myProjects = [
         list: ['Initial contacts are uploaded from data.json',
             'Every new contact is saved on local storage'],
         tools: 'React.js, React Hooks, React Context, npm, Material UI, local storage',
-        img: 'contacts'
+        img: 'contacts',
+        codeURL: 'https://github.com/klaudiaholyst/contacts-app'
     },
     {
         title: 'Calculator',
         details: 'Simple calculator made with JavaScript',
         tools: 'JavaScript, Grid CSS',
-        img: 'calculator'
+        img: 'calculator',
+        codeURL: 'https://github.com/klaudiaholyst/Calculator',
+        liveURL: 'https://klaudiaholyst.github.io/Calculator/'
+    },
+    {
+        title: 'Responsive Website',
+        details: 'Simple responsive website. Styling done using BEM methodology.',
+        tools: 'HTML, CSS, JavaScript',
+        img: 'responsiveWebsite',
+        codeURL: 'https://github.com/klaudiaholyst/Weekly-WebDev-Challenge-2',
+        liveURL: 'https://klaudiaholyst.github.io/Weekly-WebDev-Challenge-2/'
     },
     {
         title: 'Responsive Section',
         details: 'Simple responsive four-card section made with HTML and CSS.',
         tools: 'HTML, CSS',
-        img: 'responsiveSection'
-    },
-    {
-        title: 'Flexbox Challenge',
-        details: 'Simple gallery, which shows more text when a user click on a photo.',
-        tools: 'HTML, CSS, FlexBox',
-        img: 'flexboxChallenge'
+        img: 'responsiveSection',
+        codeURL: 'https://github.com/klaudiaholyst/-Four-card-feature-section-challenge-from-Frontend-Mentor-',
+        liveURL: 'https://four-card-feature-section-challenge-from-frontend-mentor.vercel.app/'
     },
     {
         title: 'Forms',
         details: 'Responsive page with forms. Used BEM methodology.',
         tools: 'HTML, CSS, BEM',
-        img: 'forms'
+        img: 'forms',
+        codeURL: 'https://github.com/klaudiaholyst/Weekly-WebDev-Challenge-1',
+        liveURL: 'https://klaudiaholyst.github.io/Weekly-WebDev-Challenge-1/'
     },
 ]
 const Projects = () => {
@@ -61,6 +72,8 @@ const Projects = () => {
                     list={project.list}
                     tools={project.tools}
                     img={project.img}
+                    codeURL={project.codeURL}
+                    liveURL={project.liveURL}
                     key={index} />
             })}
         </main>
